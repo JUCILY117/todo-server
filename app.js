c147fd30fc93d962941f59786d54009a0ba4a572
Aniket <111986929+JUCILY117@users.noreply.github.com>
2024-05-21 18:25:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.post("/login-user", async (req, res) => {
   if (!user) {
     return res.json({ error: "User Not found" });
   }
-  if (password, user.password)) {
+  if (password, user.password) {
     const token = jwt.sign({ email: user.email }, JWT_SECRET, {
       expiresIn: "15m",
     });
